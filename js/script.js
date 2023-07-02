92a9292b004aa1cf234c221b0f0c187ad24d0297
hakobya4 <108638724+hakobya4@users.noreply.github.com>
2023-07-02 14:59:50 -0400

    Added a loop to print out names and heights of pokemons

--- js/script.js
+++ js/script.js
@@ -15,3 +15,13 @@ let pokemonList = [
         type: 'fairy'
     },
 ]
+
+//loops over the pokemonlist array and prints out  the name and height of each porkemon
+for (let i=0; i<pokemonList.length; i++){
+    document.write(pokemonList[i].name + ' (height: ' +pokemonList[i].height + ') ')
+    //if pokemon's height is bigger than 20 it is big
+    if (pokemonList[i].height> 20){
+        document.write('- Wow thats big! ')
+    }
+    document.write('<br>')
+}
