9798c8d64b2b0235d5d1b30422aecfc378242fec
hakobya4 <108638724+hakobya4@users.noreply.github.com>
2023-07-02 23:35:51 -0400

    wrapped pokemonList array with IIFE to avoid accidentally accessing the global state

--- js/script.js
+++ js/script.js
@@ -1,27 +1,40 @@
-let pokemonList = [
-    {
+let pokemonRepository =(function(){
+    let pokemonList = [
+        {
         name: 'Butterfree',
         height: 12,
         type: ['bug', 'flying']
-    },
-    {
+        },
+        {
         name: 'Fearow',
         height: 22,
         type: ['normal', 'flying']
-    },
-    {
+        },
+        {
         name: 'Clefairy',
         height: 35,
         type: 'fairy'
-    },
-]
+        },
+    ];
+
+    //functions to add to the pokemon array and 
+    return {
+        add: function(pokemon) {
+          pokemonList.push(pokemon);
+        },
+        getAll: function() {
+          return pokemonList;
+        }
+      };
+})();
+
 
 //loops over the pokemonlist array and prints out  the name and height of each porkemon
-pokemonList.forEach(function(pokemon){
+pokemonRepository.getAll().forEach(function(pokemon){
     document.write(pokemon.name + ' (height: ' +pokemon.height + ') ')
     //if pokemon's height is bigger than 20 it is big
     if (pokemon.height> 20){
         document.write('- Wow thats big! ')
     }
     document.write('<br>')
-})()
+})
